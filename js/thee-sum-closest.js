74dca5db7f642e56027fafebb3371402bbacba30
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-11-06 11:09:07 +0530

    Added 3 sum logic losest logic

--- js/thee-sum-closest.js
+++ js/thee-sum-closest.js
@@ -16,6 +16,27 @@ var threeSumClosest = function (nums, target) {
   // 7. Calculate who os closer ...prev closest or total,  closest => if target - closest < target - total then closest else total
   // 8. If total < target then increment j, skip duplicate
   // 9. else decreemnt k
-  let closest = 0;
+  let closest = Infinity;
   nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i - 1] === nums[i]) continue;
+
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum === target) return target;
+
+      if (Math.abs(sum - target) < Math.abs(closest - target)) closest = sum;
+
+      if (sum < target) j++;
+      else k--;
+    }
+  }
+  return closest;
 };
+
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+console.log(threeSumClosest([-1, 2, 1, -4], 1));
