d389ee94ab50c790f305fd23b84730beba08d9c7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-12-07 12:26:08 +0530

    Aded comment

--- js/count-and.say.js
+++ js/count-and.say.js
@@ -3,6 +3,15 @@
  * @return {string}
  */
 var countAndSay = function (n) {
+  /**
+   * Algorithm
+   * 
+   * 1. Initial case 1- > 1, 2 -> 11  and initialise previousString as 11 initially
+   * 2. From n=3 Use a loop to iterate until counter is less then n
+   * 3. Use another for loop to iterate hough previousString
+   * 4. Set the count as 1, Run a while loop to check if there are multiple same digits and accoinly increment second loop ounter and count
+   * 5. Update the previousString by appending 'count actual Value (Use a temp variable to prevent infinite loop for previousString'
+   */
   if (n === 1) return '1';
   if (n === 2) return '11';
   let res = '11';
@@ -23,10 +32,10 @@ var countAndSay = function (n) {
   return res;
 };
 // 1
-// console.log(countAndSay(1));
+console.log(countAndSay(1));
 // 11
-// console.log(countAndSay(2));
+console.log(countAndSay(2));
 // 21
-// console.log(countAndSay(3));
+console.log(countAndSay(3));
 // 1211
 console.log(countAndSay(4));
